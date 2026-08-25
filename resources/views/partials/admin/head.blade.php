745b3b3547b24938319a890932572229fa673dac
Vaibhav Kondekar <mail@virratglobal.com>
2026-08-25 22:28:32 +0530

    Revert "UI: Apply Precision Engineering Design System and redesign Stores Management table"
    This reverts commit 50677e3c6154378de5e36145a60d909bb7c9f8c4.

--- resources/views/partials/admin/head.blade.php
+++ resources/views/partials/admin/head.blade.php
@@ -61,11 +61,5 @@
 
     <link rel="stylesheet" href="{{ asset('css/custom-color.css') }}">
     
-    <!-- Precision Engineering Design System -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Geist:wght@400;500;600;700&family=Inter:wght@400;500;600&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/precision-engineering.css') }}">
-    
     @stack('css')
 </head>
